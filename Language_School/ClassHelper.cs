f1b5be220fe154b304e122120f8d3853e7b78895
Nikitosik11263 <80972952+Nikitosik11263@users.noreply.github.com>
2022-05-19 13:19:01 +0300

    Добавьте файлы проекта.

---
+++ Language_School/ClassHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Language_School.DB;
+
+namespace Language_School
+{
+    class ClassHelper
+    {
+        public static Entities context { get; set; } = new Entities();
+    }
+}
